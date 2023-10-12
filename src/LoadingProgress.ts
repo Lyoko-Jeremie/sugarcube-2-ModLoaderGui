e0f10adc43a7e22538a1f60ac07bd9060364c200
LyokoJeremie <lucheng989898@gmail.com>
2023-10-12 08:15:58 +0800

    LoadingProgress

--- src/LoadingProgress.ts
+++ src/LoadingProgress.ts
@@ -2,6 +2,8 @@ import type {SC2DataManager} from "../../../dist-BeforeSC2/SC2DataManager";
 import type {ModUtils} from "../../../dist-BeforeSC2/Utils";
 import type {LifeTimeCircleHook} from "../../../dist-BeforeSC2/ModLoadController";
 import moment from "moment";
+import {Subject} from 'rxjs';
+import {bufferTime, map} from 'rxjs/operators';
 
 export interface LogItem {
     time: moment.Moment;
@@ -15,6 +17,28 @@ export class LoadingProgress implements LifeTimeCircleHook {
         public gModUtils: ModUtils,
     ) {
         this.init();
+        this.logSubject.pipe(
+            map(T => {
+                this.logList.push(T);
+                return this.LogItem2Node(T);
+            }),
+            bufferTime(100),
+        ).subscribe({
+            next: (T) => {
+                // if (this.logNode) {
+                //     this.logNode.append(...T);
+                // }
+                if (this.logNode) {
+                    this.logNode.innerHTML = '';
+                    this.logNode.append(...this.getLoadLogHtml());
+                }
+            },
+            error: (e) => {
+                console.error('LoadingProgress logSubject error', e);
+            },
+            complete: () => {
+            },
+        });
     }
 
     initOk = false;
@@ -31,9 +55,10 @@ export class LoadingProgress implements LifeTimeCircleHook {
         this.logNode = document.createElement('div');
         this.logNode.id = 'LoadingProgressLog';
         this.logNode.innerText = '';
-        this.logNode.style.cssText = 'position: fixed;left: 1px;bottom: calc(1px + 1em);' +
+        this.logNode.style.cssText = 'position: fixed;left: 1px;bottom: calc(1px + 3em);' +
             'font-size: .75em;z-index: 500001;user-select: none;' +
-            'border: gray dashed 2px;color: gray;padding: .25em;';
+            'border: gray dashed 2px;color: gray;padding: .25em;' +
+            'pointer-events: none;';
         document.body.appendChild(this.logNode);
     }
 
@@ -48,15 +73,21 @@ export class LoadingProgress implements LifeTimeCircleHook {
 
     logList: LogItem[] = [];
 
+    logSubject = new Subject<LogItem>();
+
+    LogItem2Node(T: LogItem) {
+        const time = T.time.format('HH:mm:ss.SSS');
+        const type = T.type;
+        const str = T.str;
+        const node = document.createElement('div');
+        node.style.cssText = `color: ${type === 'error' ? 'red' : type === 'warning' ? 'orange' : 'gray'};`;
+        node.innerText = `${time} ${str}`;
+        return node;
+    }
+
     getLoadLogHtml() {
         return this.logList.map(T => {
-            const time = T.time.format('HH:mm:ss.SSS');
-            const type = T.type;
-            const str = T.str;
-            const node = document.createElement('div');
-            node.style.cssText = `color: ${type === 'error' ? 'red' : type === 'warning' ? 'orange' : 'gray'};`;
-            node.innerText = `${time} ${str}`;
-            return node;
+            return this.LogItem2Node(T);
         });
     }
 
@@ -66,125 +97,111 @@ export class LoadingProgress implements LifeTimeCircleHook {
         });
     }
 
-    update() {
-        if (this.logNode) {
-            this.logNode.innerHTML = '';
-            this.logNode.append(...this.getLoadLogHtml());
-        }
-    }
+    // update() {
+    //     if (this.logNode) {
+    //         this.logNode.innerHTML = '';
+    //         this.logNode.append(...this.getLoadLogHtml());
+    //     }
+    // }
 
     async InjectEarlyLoad_start(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `InjectEarlyLoad_start [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async InjectEarlyLoad_end(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `InjectEarlyLoad_  end [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async EarlyLoad_start(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `EarlyLoad_start [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async EarlyLoad_end(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `EarlyLoad_  end [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async Load_start(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `Load_start [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async Load_end(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `Load_  end [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async PatchModToGame_start() {
-        this.logList.push({
+        this.logSubject.next({
             str: `PatchModToGame_start`,
             time: moment(),
         });
-        this.update();
     }
 
     async PatchModToGame_end() {
-        this.logList.push({
+        this.logSubject.next({
             str: `PatchModToGame_  end`,
             time: moment(),
         });
-        this.update();
     }
 
     async ReplacePatcher_start(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `ReplacePatcher_start [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async ReplacePatcher_end(modName: string, fileName: string) {
-        this.logList.push({
+        this.logSubject.next({
             str: `ReplacePatcher_  end [${modName}] [${fileName}]`,
             time: moment(),
         });
-        this.update();
     }
 
     async ModLoaderLoadEnd() {
-        this.logList.push({
+        this.logSubject.next({
             str: `ModLoaderLoadEnd.  Press [Alt+M] to open ModLoader GUI. Press [Ctrl+Alt+M] to dump data.`,
             time: moment(),
         });
-        this.update();
     }
 
     logError(s: string): void {
-        this.logList.push({
+        this.logSubject.next({
             str: `[[logError]] ${s}`,
             time: moment(),
             type: 'error',
         });
-        this.update();
     }
 
     logInfo(s: string): void {
-        this.logList.push({
+        this.logSubject.next({
             str: `[[logInfo]] ${s}`,
             time: moment(),
             type: 'info',
         });
-        this.update();
     }
 
     logWarning(s: string): void {
-        this.logList.push({
+        this.logSubject.next({
             str: `[[logWarning]] ${s}`,
             time: moment(),
             type: 'warning',
         });
-        this.update();
     }
 }
